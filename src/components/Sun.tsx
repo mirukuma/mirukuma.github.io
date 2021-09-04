e2238fcfb18ebb0d883a67acd5954df49645f7e6
mirukuma <mirukuma_tech@icloud.com>
2021-09-05 01:01:56 +0900

    太陽の位置が時間によって変わるように

--- src/components/Sun.tsx
+++ src/components/Sun.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import SunCalc from "suncalc";
+
 const Sun = () => {
   const sunColorList = ['#EE8', '#EE8', '#E64', '#EEE', '#EEE', '#EEE', '#E64',' #EE8'];
   const time = new Date();
   const colorIndex = Math.round(time.getHours() / 3) % 8;
   const sunColor = sunColorList[colorIndex];
+  const times = SunCalc.getTimes(time, 35.0302644, 135.7858041);
+  const sunrise = times.sunrise;
+  const sunset = times.sunset;
+  const sunPosition = (time.getTime() - sunrise.getTime()) / (sunset.getTime() - sunrise.getTime())
+  const x = 50 - 40 * Math.cos(Math.PI * sunPosition);
+  const y = 80 - 75 * Math.sin(Math.PI * sunPosition);
+  console.log(y)
   return (
     <div class="sun" style ={
       { 
         position: "fixed",
         borderRadius: "50%",
-        left: "80%",
-        top: "20%",
+        left: `${x}%`,
+        top: `${y}%`,
         width: "7vh",
         height: "7vh",
         background: `${sunColor}`,
