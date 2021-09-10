7af81870113bd78c523761efd17f4008036d64fe
mirukuma <mirukuma_tech@icloud.com>
2021-09-11 03:02:39 +0900

    とりあえずworksを追加

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -5,10 +5,25 @@ import PageTemplate from '../components/PageTemplate'
 
 import MainText from '../components/MainText'
 
+import CGGImage from '../assets/img/works/CuteGirlsGenerator_blur.png'
+
 const Works = () => (
   <>
     <PageTemplate contents='Works' />
-    <MainText contents='Coming soon......'/> 
+    <MainText
+      contents={(
+        <div class="works">
+          <a href="https://drive.google.com/file/d/1h84AgKdarTTg6dSXYdphnQ7xncjr-h9p/view" target="_blank">
+              <img src={CGGImage} />
+              <h1>Cute Girls Generator</h1>
+              <p>Cute Girls Generator(CGG) is a service that generates cute and erotic girls with machine learning. 
+              To make it, I teamed up with the most talented people I've ever met(The origin of DEXUS!).  
+              It is still working progress. <br/>
+              CLICK TO SEE DEMO MOVIE.</p>> 
+          </a>
+        </div>
+      )}
+    />
   </>
 );
 
